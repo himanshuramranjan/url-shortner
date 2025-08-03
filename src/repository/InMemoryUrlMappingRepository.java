fc53b6fb7bec880c3f97a20eb311c81ae776f3fa
Himanshuram Ranjan <himanshuramr@gmail.com>
2025-08-03 22:33:16 +0530

    added method to remove the stale url

--- src/repository/InMemoryUrlMappingRepository.java
+++ src/repository/InMemoryUrlMappingRepository.java
@@ -8,9 +8,11 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public class InMemoryUrlMappingRepository implements UrlMappingRepository {
     private final Map<String, UrlMapping> urlStorage = new ConcurrentHashMap<>();
+    private final Map<String, UrlMapping> originalUrlStorage = new ConcurrentHashMap<>();
     @Override
     public void save(UrlMapping urlMapping) {
         urlStorage.put(urlMapping.getShortCode(), urlMapping);
+        originalUrlStorage.put(urlMapping.getOriginalUrl(), urlMapping);
     }
 
     @Override
@@ -18,8 +20,22 @@ public class InMemoryUrlMappingRepository implements UrlMappingRepository {
         return Optional.ofNullable(urlStorage.get(shortCode));
     }
 
+    public Optional<UrlMapping> findByOriginalUrl(String originalUrl) {
+        return Optional.ofNullable(originalUrlStorage.get(originalUrl));
+    }
+
     @Override
     public boolean exists(String code) {
         return urlStorage.containsKey(code);
+    }
+
+    @Override
+    public boolean remove(String shortCode) {
+        UrlMapping mapping = urlStorage.remove(shortCode);
+        if (mapping != null) {
+            originalUrlStorage.remove(mapping.getOriginalUrl());
+            return true;
+        }
+        return false;
     }
 }
--- src/repository/UrlMappingRepository.java
+++ src/repository/UrlMappingRepository.java
@@ -7,5 +7,7 @@ import java.util.Optional;
 public interface UrlMappingRepository {
     void save(UrlMapping urlMapping);
     Optional<UrlMapping> findByShortCode(String shortCode);
+    Optional<UrlMapping> findByOriginalUrl(String originalUrl);
     boolean exists(String code);
+    boolean remove(String shortCode);
 }
